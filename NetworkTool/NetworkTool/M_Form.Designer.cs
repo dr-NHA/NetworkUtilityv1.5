692b638340df412082618339c635437ef55c089d
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2021-03-16 01:13:31 +1030

    Add files via upload

---
+++ NetworkTool/NetworkTool/M_Form.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace NetworkTool
+{
+    partial class M_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DBG_ = new System.Windows.Forms.TextBox();
+            this.GetAllDataOut = new System.Windows.Forms.Button();
+            this.DisableAllNetworks = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.EnableAllNetworks = new System.Windows.Forms.Button();
+            this.ShowWifiAndEnthernetMac = new System.Windows.Forms.Button();
+            this.SpoofAll = new System.Windows.Forms.Button();
+            this.RESETMAC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DBG_
+            // 
+            this.DBG_.Dock = System.Windows.Forms.DockStyle.Right;
+            this.DBG_.Location = new System.Drawing.Point(427, 0);
+            this.DBG_.MinimumSize = new System.Drawing.Size(373, 450);
+            this.DBG_.Multiline = true;
+            this.DBG_.Name = "DBG_";
+            this.DBG_.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.DBG_.ShortcutsEnabled = false;
+            this.DBG_.Size = new System.Drawing.Size(373, 450);
+            this.DBG_.TabIndex = 1;
+            this.DBG_.WordWrap = false;
+            // 
+            // GetAllDataOut
+            // 
+            this.GetAllDataOut.Location = new System.Drawing.Point(12, 12);
+            this.GetAllDataOut.Name = "GetAllDataOut";
+            this.GetAllDataOut.Size = new System.Drawing.Size(362, 191);
+            this.GetAllDataOut.TabIndex = 2;
+            this.GetAllDataOut.Text = "Get Net Info From System";
+            this.GetAllDataOut.UseVisualStyleBackColor = true;
+            this.GetAllDataOut.Click += new System.EventHandler(this.GetAllDataOut_Click);
+            // 
+            // DisableAllNetworks
+            // 
+            this.DisableAllNetworks.Location = new System.Drawing.Point(47, 220);
+            this.DisableAllNetworks.Name = "DisableAllNetworks";
+            this.DisableAllNetworks.Size = new System.Drawing.Size(292, 23);
+            this.DisableAllNetworks.TabIndex = 3;
+            this.DisableAllNetworks.Text = "Disable All WIFI/ENTHERNET Interface";
+            this.DisableAllNetworks.UseVisualStyleBackColor = true;
+            this.DisableAllNetworks.Click += new System.EventHandler(this.DisableAllNetworks_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter1.Location = new System.Drawing.Point(424, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 450);
+            this.splitter1.TabIndex = 5;
+            this.splitter1.TabStop = false;
+            // 
+            // EnableAllNetworks
+            // 
+            this.EnableAllNetworks.Location = new System.Drawing.Point(47, 249);
+            this.EnableAllNetworks.Name = "EnableAllNetworks";
+            this.EnableAllNetworks.Size = new System.Drawing.Size(292, 23);
+            this.EnableAllNetworks.TabIndex = 6;
+            this.EnableAllNetworks.Text = "Enable All WIFI/ENTHERNET Interface";
+            this.EnableAllNetworks.UseVisualStyleBackColor = true;
+            this.EnableAllNetworks.Click += new System.EventHandler(this.EnableAllNetworks_Click);
+            // 
+            // ShowWifiAndEnthernetMac
+            // 
+            this.ShowWifiAndEnthernetMac.Location = new System.Drawing.Point(12, 304);
+            this.ShowWifiAndEnthernetMac.Name = "ShowWifiAndEnthernetMac";
+            this.ShowWifiAndEnthernetMac.Size = new System.Drawing.Size(383, 51);
+            this.ShowWifiAndEnthernetMac.TabIndex = 7;
+            this.ShowWifiAndEnthernetMac.Text = "Get Mac Address Infomation";
+            this.ShowWifiAndEnthernetMac.UseVisualStyleBackColor = true;
+            this.ShowWifiAndEnthernetMac.Click += new System.EventHandler(this.SpoofWifiAndEnthernet_Click);
+            // 
+            // SpoofAll
+            // 
+            this.SpoofAll.Location = new System.Drawing.Point(12, 362);
+            this.SpoofAll.Name = "SpoofAll";
+            this.SpoofAll.Size = new System.Drawing.Size(383, 37);
+            this.SpoofAll.TabIndex = 8;
+            this.SpoofAll.Text = "Spoof All WIFI/ENTHERNET Interface Mac Address\'s";
+            this.SpoofAll.UseVisualStyleBackColor = true;
+            this.SpoofAll.Click += new System.EventHandler(this.SpoofAll_Click);
+            // 
+            // RESETMAC
+            // 
+            this.RESETMAC.Location = new System.Drawing.Point(12, 403);
+            this.RESETMAC.Name = "RESETMAC";
+            this.RESETMAC.Size = new System.Drawing.Size(383, 37);
+            this.RESETMAC.TabIndex = 9;
+            this.RESETMAC.Text = "Reset All WIFI/ENTHERNET Interface Mac Address\'s";
+            this.RESETMAC.UseVisualStyleBackColor = true;
+            this.RESETMAC.Click += new System.EventHandler(this.RESETMAC_Click);
+            // 
+            // M_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RESETMAC);
+            this.Controls.Add(this.SpoofAll);
+            this.Controls.Add(this.ShowWifiAndEnthernetMac);
+            this.Controls.Add(this.EnableAllNetworks);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.DisableAllNetworks);
+            this.Controls.Add(this.GetAllDataOut);
+            this.Controls.Add(this.DBG_);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "M_Form";
+            this.Text = "NHA | Network Tool V0.1b";
+            this.Load += new System.EventHandler(this.M_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox DBG_;
+        private System.Windows.Forms.Button GetAllDataOut;
+        private System.Windows.Forms.Button DisableAllNetworks;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Button EnableAllNetworks;
+        private System.Windows.Forms.Button ShowWifiAndEnthernetMac;
+        private System.Windows.Forms.Button SpoofAll;
+        private System.Windows.Forms.Button RESETMAC;
+    }
+}
+
---
+++ NetworkTool/NetworkTool/M_Form.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace NetworkTool{
+public partial class M_Form : Form{
+public M_Form(){
+InitializeComponent();
+}
+
+private void M_Form_Load(object sender, EventArgs e){
+List<string> DBGN=new List<string>();
+DBGN.Add("Enabled:");
+foreach(string inst in GetEnabledInstances()){
+DBGN.Add(inst);
+}
+DBGN.Add(" ");
+DBGN.Add("Disabled:");
+foreach(string inst in GetDisabledInstances()){
+DBGN.Add(inst);
+}
+
+DBG_.Lines = DBGN.ToArray();
+}
+
+
+string[] GetNetInfoFromMachine(string RetTipe){ string[] rettie = { "No Valid Return Type" };
+List<string> Names = new List<string>();
+List<string> Descriptions = new List<string>();
+List<string> IDS = new List<string>();
+List<string> Addresses = new List<string>();
+foreach (NetworkInterface NetTyin in NetworkInterface.GetAllNetworkInterfaces()){
+Names.Add(NetTyin.Name);
+Descriptions.Add(NetTyin.Description);
+IDS.Add(NetTyin.Id);
+Addresses.Add(NetTyin.GetPhysicalAddress().ToString());
+}
+
+
+
+
+List<string> RegisNames = new List<string>();
+List<string> PnPInstanceId = new List<string>();
+List<string> MediaSubType=new List<string>();
+List<string> MST_Parent=new List<string>();
+
+//We Will Use Method 1 As It Saves Performance And Time, Everyone Else Uses Method 2 short XD
+
+
+//method 1
+//RegistryKey OurNetInfo=Registry.LocalMachine.OpenSubKey("SYSTEM").OpenSubKey("CurrentControlSet").OpenSubKey("Control").OpenSubKey("Network");
+//method 1 short
+RegistryKey OurNetInfo=Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Network",true);
+//method 2
+//RegistryKey OurNetInfo=RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,RegistryView.Registry32).OpenSubKey("SYSTEM").OpenSubKey("CurrentControlSet").OpenSubKey("Control").OpenSubKey("Network");
+//method 2 short
+//RegistryKey OurNetInfo=RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,RegistryView.Registry32).OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Network",true);
+
+List<string> NotRegistryKeyHives=new List<string>() { "null",""," ","Connections", "Interfaces" , "LightweightCallHandlers", "NetworkLocationWizard", "SharedAccessConnection" };
+
+RegistryKey NetRegLoc=RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,RegistryView.Registry32);
+
+foreach(string Kei in OurNetInfo.GetSubKeyNames()){
+string[] OPos= Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Network\" + Kei, true).GetSubKeyNames();
+if (!NotRegistryKeyHives.Contains(Kei)&&OPos.Contains("Descriptions")){
+NetRegLoc=Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Network\"+Kei, true);
+}
+}
+
+List<RegistryKey> NetKeyList=new List<RegistryKey>();
+List<string> Gret=new List<string>();
+foreach(string Possi in NetRegLoc.GetSubKeyNames()){
+if(Possi!="Descriptions"){
+RegistryKey TempNetKei= NetRegLoc.OpenSubKey(Possi+"\\Connection", true);
+string TempNetName= (string)TempNetKei.GetValue("Name", true);
+if(TempNetKei.GetValueNames().Contains("PnPInstanceId")){
+
+if(TempNetKei.GetValueNames().Contains("MediaSubType")){
+int val= (int)TempNetKei.GetValue("MediaSubType");
+MediaSubType.Add(val.ToString());
+}else{
+MediaSubType.Add("None");
+}
+
+if(Names.Contains(TempNetName)){
+Gret.Add("ENABLED");
+}else{
+Gret.Add("DISABLED");
+}
+
+NetKeyList.Add(TempNetKei);
+RegisNames.Add(TempNetName);
+PnPInstanceId.Add((string)TempNetKei.GetValue("PnPInstanceId", true));
+
+
+}
+}
+}
+
+
+
+
+
+
+
+string rexi = RetTipe.ToLower();
+if (rexi == "names"){
+rettie = Names.ToArray();
+}else if (rexi == "descriptions"){
+rettie = Descriptions.ToArray();
+}else if (rexi == "ids"){
+rettie = IDS.ToArray();
+}else if (rexi == "addresses"){
+rettie = Addresses.ToArray();
+}else if (rexi == "registrynames"){
+rettie = RegisNames.ToArray();
+}else if (rexi == "pnpinstanceid"){
+rettie = PnPInstanceId.ToArray();
+}else if (rexi == "mediasubtype"){
+rettie = MediaSubType.ToArray();
+}else if (rexi == "state"){
+rettie = Gret.ToArray();
+}
+
+
+return rettie;
+}
+
+
+
+static void EnableNetworkInterface(string interfaceName){
+ProcessStartInfo psi =new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");Process p = new Process();
+psi.CreateNoWindow = true;
+psi.WindowStyle=ProcessWindowStyle.Hidden;
+p.StartInfo = psi;
+p.Start();
+}
+
+static void DisableNetworkInterface(string interfaceName){
+ProcessStartInfo psi =new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");Process p = new Process();
+psi.CreateNoWindow = true;
+psi.WindowStyle=ProcessWindowStyle.Hidden;
+p.StartInfo = psi;
+p.Start();
+}
+
+
+string[] GetEnabledInstances(){
+string[] States = GetNetInfoFromMachine("State");
+string[] PnpInstanceIds = GetNetInfoFromMachine("pnpinstanceid");
+string[] RegisNames = GetNetInfoFromMachine("registrynames");
+List<string> Greturn = new List<string>();
+for (var i = 0; i < RegisNames.Length; i++){
+if (States[i].ToLower() == "enabled" && PnpInstanceIds[i].Trim(' ').StartsWith("PCI\\")){
+Greturn.Add(RegisNames[i]);
+}
+}
+return Greturn.ToArray();
+}
+
+string[] GetDisabledInstances(){
+string[] States = GetNetInfoFromMachine("State");
+string[] PnpInstanceIds = GetNetInfoFromMachine("pnpinstanceid");
+string[] RegisNames = GetNetInfoFromMachine("registrynames");
+List<string> Greturn = new List<string>();
+for (var i = 0; i < RegisNames.Length; i++){
+if (States[i].ToLower() == "disabled" && PnpInstanceIds[i].Trim(' ').StartsWith("PCI\\")){
+Greturn.Add(RegisNames[i]);
+}
+}
+return Greturn.ToArray();
+}
+
+
+private void DisableAllNetworks_Click(object sender, EventArgs e){
+foreach (string nam in GetEnabledInstances()){
+DisableNetworkInterface(nam);
+}
+}
+
+private void EnableAllNetworks_Click(object sender, EventArgs e){
+foreach (string nam in GetDisabledInstances()){
+EnableNetworkInterface(nam);
+}
+}
+
+
+
+
+
+private void GetAllDataOut_Click(object sender, EventArgs e){
+string[] Names = GetNetInfoFromMachine("Names");
+string[] Ids = GetNetInfoFromMachine("Ids");
+string[] Descriptions = GetNetInfoFromMachine("Descriptions");
+string[] addresses = GetNetInfoFromMachine("Addresses");
+string[] States =   GetNetInfoFromMachine("State");         
+string[] PnpInstanceIds = GetNetInfoFromMachine("pnpinstanceid");
+string[] MediaSubType = GetNetInfoFromMachine("mediasubtype");
+string[] RegisNames = GetNetInfoFromMachine("registrynames");
+List<string> Greturn = new List<string>();
+for (var i=0;i< RegisNames.Length;i++){
+Greturn.Add("Found Network: "+i.ToString());
+Greturn.Add("Registry Name: " + RegisNames[i]);
+Greturn.Add("Pnp Instance Id: "+PnpInstanceIds[i]);
+Greturn.Add("Media Sub Type: " + MediaSubType[i]);
+Greturn.Add("State: "+States[i]);
+Greturn.Add(" ");
+if(States[i].ToLower()=="enabled"){
+for(var ix=0;ix< Names.Length;ix++){
+if(Names[ix]==RegisNames[i] ){
+Greturn.Add("Name: "+Names[ix]);
+Greturn.Add("Description: " + Descriptions[ix]);
+Greturn.Add("Id: " + Ids[ix]);
+Greturn.Add("Address: " + addresses[ix]);
+}}
+
+}
+
+Greturn.Add(" ");
+Greturn.Add(" ");
+Greturn.Add(" ");
+}
+
+
+Greturn.RemoveAt(Greturn.Count-1);
+DBG_.Lines = Greturn.ToArray();
+}
+
+RegistryKey MacAddressKeyStore() { return Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class", true); }
+
+string NetworkKeystoreId(){
+RegistryKey RegistryItemList=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class", true);
+string keiloc = "";
+foreach(string key in RegistryItemList.GetSubKeyNames()){
+RegistryKey subkei = MacAddressKeyStore().OpenSubKey(key, true);
+if (subkei.GetValueNames().Contains("Class")){
+if((string) subkei.GetValue("Class")=="Net"){
+keiloc=key;break;
+}
+}}
+return keiloc;
+}
+
+private void SpoofWifiAndEnthernet_Click(object sender, EventArgs e){
+string keiloc= NetworkKeystoreId();
+
+List<string> MacAddresses=new List<string>();
+MacAddresses.Add(" ");
+MacAddresses.Add("!NHA MAC ADDRESS FINDING ALGO V1.5!");
+MacAddresses.Add(" ");
+MacAddresses.Add(@"Network Keys Stored In: SYSTEM\ControlSet001\Control\Class\"+keiloc);
+MacAddresses.Add(" ");
+MacAddresses.Add(" ");
+
+RegistryKey NetKeys=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class\"+ keiloc, true);
+foreach(string keystore in NetKeys.GetSubKeyNames()){
+if(keystore!="Properties"){
+RegistryKey SubKei=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class\"+ keiloc+"\\"+keystore,true);
+if(SubKei.GetValueNames().Contains("ComponentId")){
+if(((string)SubKei.GetValue("ComponentId")).Trim(' ').StartsWith("PCI\\")&&SubKei.GetValueNames().Contains("NetCfgInstanceId")){
+string MAC= ((string)SubKei.GetValue("NetCfgInstanceId")).Replace("{", "").Replace("}", "");
+string DEVICE= (string)SubKei.GetValue("DriverDesc");
+MacAddresses.Add("Network Key: "+keystore);
+MacAddresses.Add("Device Name: "+DEVICE);
+
+bool MacChanged=false;
+if(SubKei.GetValueNames().Contains("BKG_NetCfgInstanceId")){
+string MACBKG= ((string)SubKei.GetValue("BKG_NetCfgInstanceId")).Replace("{", "").Replace("}", "");
+if(MACBKG!=MAC){
+ MacChanged=true;
+MacAddresses.Add("Original MAC Address: "+ MACBKG);
+}else{
+}
+}
+           
+if(MacChanged==false){
+MacAddresses.Add("MAC Address: "+MAC);
+}else{
+MacAddresses.Add("(SPOOFED) MAC Address: "+MAC);
+}
+                            
+
+MacAddresses.Add(" ");
+}
+}}
+}
+DBG_.Lines=MacAddresses.ToArray();
+
+}
+
+private void SpoofAll_Click(object sender, EventArgs e){
+string keiloc= NetworkKeystoreId();
+
+List<string> MacAddresses=new List<string>();
+MacAddresses.Add(" ");
+MacAddresses.Add("!NHA MAC ADDRESS FINDING ALGO V1.5!");
+MacAddresses.Add(" ");
+MacAddresses.Add(@"Network Keys Stored In: SYSTEM\ControlSet001\Control\Class\"+keiloc);
+MacAddresses.Add(" ");
+MacAddresses.Add(" ");
+
+RegistryKey NetKeys=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class\"+ keiloc, true);
+foreach(string keystore in NetKeys.GetSubKeyNames()){
+if(keystore!="Properties"){
+RegistryKey SubKei=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class\"+ keiloc+"\\"+keystore,true);
+if(SubKei.GetValueNames().Contains("ComponentId")){
+if(((string)SubKei.GetValue("ComponentId")).Trim(' ').StartsWith("PCI\\")&&SubKei.GetValueNames().Contains("NetCfgInstanceId")){
+string MAC= ((string)SubKei.GetValue("NetCfgInstanceId")).Replace("{", "").Replace("}", "");
+string NEWMAC= CreateNewMACAddress();
+string DEVICE= (string)SubKei.GetValue("DriverDesc");
+if(!SubKei.GetValueNames().Contains("BKG_NetCfgInstanceId")){
+SubKei.SetValue("BKG_NetCfgInstanceId","{"+MAC+"}");
+}
+
+MacAddresses.Add("Network Key: "+keystore);
+MacAddresses.Add("Device Name: "+DEVICE);
+MacAddresses.Add("*OLD MAC Address: "+MAC);
+MacAddresses.Add("NEW! MAC Address: "+NEWMAC.Replace("{","").Replace("}",""));
+MacAddresses.Add(" ");
+if(SubKei.GetValueNames().Contains("BKG_NetCfgInstanceId")){SubKei.SetValue("NetCfgInstanceId",NEWMAC);}
+
+}
+}}
+}
+DBG_.Lines=MacAddresses.ToArray();
+
+}
+
+
+
+private void RESETMAC_Click(object sender, EventArgs e){
+string keiloc= NetworkKeystoreId();
+
+List<string> MacAddresses=new List<string>();
+MacAddresses.Add(" ");
+MacAddresses.Add("!NHA MAC ADDRESS FINDING ALGO V1.5!");
+MacAddresses.Add(" ");
+MacAddresses.Add(@"Network Keys Stored In: SYSTEM\ControlSet001\Control\Class\"+keiloc);
+MacAddresses.Add(" ");
+MacAddresses.Add(" ");
+
+RegistryKey NetKeys=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class\"+ keiloc, true);
+foreach(string keystore in NetKeys.GetSubKeyNames()){
+if(keystore!="Properties"){
+RegistryKey SubKei=Registry.LocalMachine.OpenSubKey(@"SYSTEM\ControlSet001\Control\Class\"+ keiloc+"\\"+keystore,true);
+if(SubKei.GetValueNames().Contains("ComponentId")){
+if(((string)SubKei.GetValue("ComponentId")).Trim(' ').StartsWith("PCI\\")&&SubKei.GetValueNames().Contains("NetCfgInstanceId")){
+string MAC= ((string)SubKei.GetValue("NetCfgInstanceId")).Replace("{", "").Replace("}", "");
+if(SubKei.GetValueNames().Contains("BKG_NetCfgInstanceId")){
+string NEWMAC=(string)SubKei.GetValue("BKG_NetCfgInstanceId");
+string DEVICE= (string)SubKei.GetValue("DriverDesc");
+SubKei.SetValue("NetCfgInstanceId",NEWMAC);
+
+MacAddresses.Add("Network Key: "+keystore);
+MacAddresses.Add("Device Name: "+DEVICE);
+MacAddresses.Add("*OLD MAC Address: "+MAC);
+MacAddresses.Add("NEW! MAC Address: "+NEWMAC.Replace("{","").Replace("}",""));
+MacAddresses.Add(" ");
+
+}
+
+}
+}}
+}
+
+DBG_.Lines=MacAddresses.ToArray();
+}
+
+        string chars = "ABCDEF0123456789";
+Random random = new Random();
+string RandomChar(){
+ return chars[random.Next(chars.Length)].ToString();
+}
+
+string CreateNewMACAddress(){
+
+string startax=RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+ RandomChar();
+
+string startbx=RandomChar()+RandomChar()+RandomChar()+RandomChar();
+            
+string startcx=RandomChar()+RandomChar()+RandomChar()+RandomChar();
+            
+string startdx=RandomChar()+RandomChar()+RandomChar()+RandomChar();
+
+string startex=RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar()+RandomChar();
+
+return "{"+startax+"-"+startbx+"-"+startcx+"-"+startdx+"-"+startex+"}";
+}
+
+
+
+
+}
+}
